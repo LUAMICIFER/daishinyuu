eb511e4a4f9088576e864e0ec1743a89ef441dfb
LUAMICIFER <96227537+LUAMICIFER@users.noreply.github.com>
2022-06-03 11:20:39 +0530

    Update adminperms.py

--- wbb/utils/adminperms.py
+++ wbb/utils/adminperms.py
@@ -1,7 +1,7 @@
 import asyncio
 from pyrogram import filters
 from pyrogram.types import ChatPermissions, Message
-from TGN import pbot as app
+from wbb import pbot as app
 
 
 async def member_permissions(chat_id: int, user_id: int):
