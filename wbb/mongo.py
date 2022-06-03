e78af3b00c9b61784df61e6436ab44fca8f1a36e
LUAMICIFER <96227537+LUAMICIFER@users.noreply.github.com>
2022-06-03 17:20:31 +0530

    Wbb

--- wbb/mongo.py
+++ wbb/mongo.py
@@ -3,20 +3,20 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from TGN import MONGO_DB_URI
-from TGN.confing import get_int_key, get_str_key
+from wbb import MONGO_DB_URI
+from wbb.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "TGN"
+MONGO_DB = "wbb"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["TGN"]
+db = client["wbb"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
